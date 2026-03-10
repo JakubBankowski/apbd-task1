edce3d68f6109036f4e7cde8146645d526d512a1
JakubBankowski <s33203@LAB362-14>
2026-03-10 11:44:16 +0100

    Added choice logic

--- apbd-1/apbd-1/Player.cs
+++ apbd-1/apbd-1/Player.cs
@@ -2,9 +2,13 @@
 {
     public class Player
     {
+        public int points = 0;
+
+        public string name;
+
         public Choice currentChoice = Choice.None;
 
-        public Player() {}
+        public Player(string name) { this.name = name; }
 
         public void PickChoice()
         {
@@ -14,6 +18,89 @@
             {
                 currentChoice = Choice.None;
                 Console.WriteLine("Wrong choice.");
+            }
+        }
+
+        public void CheckChoice(Player otherPlayer) {
+            if (currentChoice == Choice.Rock) { 
+                if (otherPlayer.currentChoice == Choice.Rock)
+                {
+                    Console.WriteLine("Draw round!");
+                }
+                else if (otherPlayer.currentChoice == Choice.Paper)
+                {
+                    Console.WriteLine(otherPlayer.name + " wins!");
+                    this.points -= 1;
+                    otherPlayer.points += 1;
+                }
+                else if (otherPlayer.currentChoice == Choice.Scissors)
+                {
+                    Console.WriteLine(this.name + " wins!");
+                    this.points += 1;
+                    otherPlayer.points -= 1;
+                }
+                else if (otherPlayer.currentChoice == Choice.None)
+                {
+                    Console.WriteLine(this.name + " wins!");
+                    this.points += 1;
+                    otherPlayer.points -= 1;
+                }
+            }
+            else if (currentChoice == Choice.Paper)
+            {
+                if (otherPlayer.currentChoice == Choice.Paper)
+                {
+                    Console.WriteLine("Draw round!");
+                }
+                else if (otherPlayer.currentChoice == Choice.Scissors)
+                {
+                    Console.WriteLine(otherPlayer.name + " wins!");
+                    this.points -= 1;
+                    otherPlayer.points += 1;
+                }
+                else if (otherPlayer.currentChoice == Choice.Rock)
+                {
+                    Console.WriteLine(this.name + " wins!");
+                    this.points += 1;
+                    otherPlayer.points -= 1;
+                }
+                else if (otherPlayer.currentChoice == Choice.None)
+                {
+                    Console.WriteLine(this.name + " wins!");
+                    this.points += 1;
+                    otherPlayer.points -= 1;
+                }
+            }
+            else if (currentChoice == Choice.Scissors)
+            {
+                if (otherPlayer.currentChoice == Choice.Scissors)
+                {
+                    Console.WriteLine("Draw round!");
+                }
+                else if (otherPlayer.currentChoice == Choice.Rock)
+                {
+                    Console.WriteLine(otherPlayer.name + " wins!");
+                    this.points -= 1;
+                    otherPlayer.points += 1;
+                }
+                else if (otherPlayer.currentChoice == Choice.Paper)
+                {
+                    Console.WriteLine(this.name + " wins!");
+                    this.points += 1;
+                    otherPlayer.points -= 1;
+                }
+                else if (otherPlayer.currentChoice == Choice.None)
+                {
+                    Console.WriteLine(this.name + " wins!");
+                    this.points += 1;
+                    otherPlayer.points -= 1;
+                }
+            }
+            else if (currentChoice == Choice.None)
+            {
+                Console.WriteLine(otherPlayer.name + " wins!");
+                this.points -= 1;
+                otherPlayer.points += 1;
             }
         }
     }
--- apbd-1/apbd-1/Program.cs
+++ apbd-1/apbd-1/Program.cs
@@ -4,13 +4,13 @@
     {
         static void Main(string[] args)
         {
-            Player player1 = new Player();
-            Player player2 = new Player();
+            Player player1 = new Player("gracz1");
+            Player player2 = new Player("gracz2");
 
-            while (true) { 
+            while (true) {
+                Console.WriteLine("Welcome to Rock, Paper, Scissors!");
+
             }
-
-            Console.WriteLine("Hello");
         }
     }
 }
